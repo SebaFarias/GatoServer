386e5d429dbdfc1e7caecceb3699c02ed80daa14
S F <sebastian.fariasb@usach.cl>
2020-10-14 20:53:34 -0300

    catch statement for DB connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const app = require('./app')
 require('./database')
 
-const port = process.env.PORT || 8080
+const server_port = process.env.YOUR_PORT || process.env.PORT || 8080
+const server_host = process.env.YOUR_HOST || '0.0.0.0';
 
-
-app.listen(port, ()=>{
-    console.log(`Listening on http://localhost:${port}`)
+app.listen(server_port, server_host ,()=>{
+    console.log(`Listening on ${server_host}:${server_port}`)
+})
+.catch( error => {
+    console.log(error);
 })
