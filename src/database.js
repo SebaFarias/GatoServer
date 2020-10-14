860701e2fdd6e1b0e30e961c8c7730e37ef06a52
S F <sebastian.fariasb@usach.cl>
2020-10-14 18:52:36 -0300

    env test

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-
+console.log(process.env.DB_CONNECTION)
 mongoose.connect(
     process.env.DB_CONNECTION,
     {useNewUrlParser: true , useUnifiedTopology: true},
