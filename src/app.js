85d4407f886cea5af025f82330b68025cf197313
S F <sebastian.fariasb@usach.cl>
2020-10-16 17:25:09 -0300

    helmet added

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const cors = require('cors')
+const helmet = require('helmet')
 const connectionRoutes = require('./routes/connection.routes')
 const movesRoutes = require('./routes/moves.routes')
 const app = express()
 
 app.use(cors())
+app.use(helmet())
 app.use(express.json())
 
 app.use('/api/v1/connection/',connectionRoutes)
 app.use('/api/v1/moves/',movesRoutes)
 
 app.get('/', (req,res) => {
-    res.json({
-        msg: 'Estamos Funcionando!'
-    })
+    res.json({msg: 'Estamos Funcionando!'})
+})
+app.get('/favicon.ico', (req,res) => {
+    res.sendFile(`${process.cwd()}/Gato.ico`)
 })
 
 module.exports = app
